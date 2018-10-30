deda72ccd2967149e1b1ac7906994fec11de31eb
SeamasShih <seamas0716@gmail.com>
2018-10-30 08:32:33 +0800

    Scrolling text can move more smoothly
    Change View to SurfaceView

--- app/src/main/java/com/honhai/foxconn/mycolorpicker/display/DisplayView.java
+++ app/src/main/java/com/honhai/foxconn/mycolorpicker/display/DisplayView.java
@@ -7,13 +7,14 @@ import android.graphics.Paint;
 import android.content.res.Resources;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.animation.LinearInterpolator;
 
-public class DisplayView extends View {
+public class DisplayView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
 
     private Paint textPaint = new Paint();
+    private int backgroundColor;
     private float adjY;
     private float x;
     private String text = "";
@@ -24,8 +25,19 @@ public class DisplayView extends View {
     private int screenWidth = dm.widthPixels;
     private int screenHeight = dm.heightPixels;
 
+    private boolean mIsDrawing;
+    private SurfaceHolder mSurfaceHolder;
+    private Canvas mCanvas;
+
     public DisplayView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mSurfaceHolder = getHolder();
+        mSurfaceHolder.addCallback(this);
+
+        setFocusable(true);
+        setKeepScreenOn(true);
+        setFocusableInTouchMode(true);
+
         textPaint.setAntiAlias(true);
         textPaint.setTextAlign(Paint.Align.LEFT);
         textPaint.setTextSize(screenHeight);
@@ -40,8 +52,12 @@ public class DisplayView extends View {
         animator.setInterpolator(new LinearInterpolator());
         animator.addUpdateListener(animation -> {
             x = (float) animation.getAnimatedValue();
-            invalidate();
         });
+    }
+
+    @Override
+    public void setBackgroundColor(int color) {
+        backgroundColor = color;
     }
 
     public void setText(String text) {
@@ -50,8 +66,8 @@ public class DisplayView extends View {
         this.text = text;
         animator.cancel();
         float l = textPaint.measureText(text);
-        animator.setFloatValues(screenWidth, -l);
-        animator.setDuration((long) (l + screenWidth));
+        animator.setFloatValues(screenWidth+20, -l-20);
+        animator.setDuration((long) (l + screenWidth)*2/3);
         animator.start();
     }
 
@@ -60,8 +76,40 @@ public class DisplayView extends View {
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawText(text, x, getHeight()/2 + adjY, textPaint);
+    public void surfaceCreated(SurfaceHolder holder) {
+        mIsDrawing = true;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mIsDrawing = false;
+    }
+
+    @Override
+    public void run() {
+        while (mIsDrawing){
+            drawSomething();
+        }
+    }
+
+    private void drawSomething() {
+        try {
+            mCanvas = mSurfaceHolder.lockCanvas();
+            if (mCanvas == null) return;
+            mCanvas.drawColor(backgroundColor);
+            mCanvas.drawText(text, x, getHeight()/2 + adjY, textPaint);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (mCanvas != null){
+                mSurfaceHolder.unlockCanvasAndPost(mCanvas);
+            }
+        }
     }
 }
